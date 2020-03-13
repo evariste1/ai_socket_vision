5008a61136c11da46f686fde6f00c44b8969b23e
evariste1 <evagkana@yahoo.com>
2020-03-13 13:28:27 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=10RBKTfg2vlqJBp3Mrklqd1ZMot5al50J'
 export_file_name = 'export.pkl'
 
 classes = ['ALBATROSS',
