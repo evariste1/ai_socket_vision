06532845a75e14ae04ea06ce0c1cf09adbf09ebe
evariste1 <evagkana@yahoo.com>
2020-04-10 08:39:28 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,8 +12,7 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://drive.google.com/uc?export=download&id=19vb4xR0J-131-ODbPRRXPlEcaAdUPpcw'
 export_file_name = 'export.pkl'
 
-classes = ['BACTERIAL OR VIRAL PNEUMONIA', 'COVID-19 INFECTION', 'HEALTHY INDIVIDUAL']
-
+classes = ['COVID-19 INFECTION', 'NO INFECTION', 'NOT CERTAIN', 'PNEUMONIA INFECTION']
 path = Path(__file__).parent
 
 app = Starlette()
