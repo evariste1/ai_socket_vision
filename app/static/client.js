aa2183012f92f242751ee6d1a3374fbb4126ff57
evariste1 <evagkana@yahoo.com>
2020-04-15 20:51:13 +0200

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -29,7 +29,7 @@ function analyze() {
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML = `My Prediction & Confidence Level(%)= ${response["result"]}`;
+      el("result-label").innerHTML = `Diagnosis & Confidence Level(%)= ${response["result"]}`;
     }
     el("analyze-button").innerHTML = "Analyze";
   };
